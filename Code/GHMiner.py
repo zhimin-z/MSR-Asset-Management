41c2e2f71b62bd23bc8b3ff8e48d6cb3ea13f433
JIMMY ZHAO <knightyzhao@gmail.com>
2022-12-11 00:34:57 -0500

    whole refactor for more organized dataset structure

--- Code/GHMiner.py
+++ Code/GHMiner.py
@@ -6,36 +6,10 @@ import time
 class GitHubMiner():
     def __init__(self, private_token):
         self.github = Github(login_or_token=private_token)
-        self.repo_columns = [
-            'Repo',
-            'Link',
-            'Archived',
-            'Creation Date',
-            'First Activity Date',
-            'Last Activity Date',
-            'Topics',
-            '#Star',
-            '#Watch',
-            '#Fork',
-            '#Releases',
-            '#Contributors',
-            'Language',
-            'Size',
-            '#Issues',
-            '#Pull Requests',
-            '#Branches',
-            '#Commits',
-            '#Comments',
-            '#Deployments'
-        ]
-        self.error_columns = [
-            'Repo',
-            'Error'
-        ]
 
-    def scrape(self, repo_name, name=None, date=None):
+    def scrape(self, repo_name, tool_release_date=None):
         def sleep_wrapper(func, **args):
-            time.sleep(5)
+            time.sleep(3)
             return func(**args)
 
         try:
@@ -43,51 +17,61 @@ class GitHubMiner():
                                  full_name_or_id=repo_name)
 
             commits = sleep_wrapper(repo.get_commits)
-            last_commit_date = pd.to_datetime(commits[0].commit.author.date).to_datetime64()
-            if date is not None and date > last_commit_date:
+            last_commit_date = pd.to_datetime(
+                commits[0].commit.author.date).to_datetime64()
+            if tool_release_date is not None and tool_release_date > last_commit_date:
                 error_data = {'Repo': repo_name, 'Error': 'Unrelevant'}
                 return None, error_data
+
+            releases = sleep_wrapper(repo.get_releases)
+            issues = sleep_wrapper(repo.get_issues, state='all')
 
             repo_data = {
                 'Repo': repo_name,
                 'Link': repo.html_url,
-                'Archived': repo.archived,
-                'Creation Date': pd.to_datetime(repo.created_at).to_datetime64(),
-                'First Activity Date': pd.to_datetime(commits.reversed[0].commit.author.date).to_datetime64(),
-                'Last Activity Date': last_commit_date,
+                'Repo Creation Date': pd.to_datetime(repo.created_at).to_datetime64(),
+                'Last Commit Date': last_commit_date,
                 'Topics': sleep_wrapper(repo.get_topics),
+                'Language': repo.language,
+                'Size': repo.size,
                 '#Star': repo.stargazers_count,
                 '#Watch': repo.subscribers_count,
                 '#Fork': repo.forks,
-                '#Releases': sleep_wrapper(repo.get_releases).totalCount,
+                '#Releases': releases.totalCount,
                 '#Contributors': sleep_wrapper(repo.get_contributors).totalCount,
-                'Language': repo.language,
-                'Size': repo.size,
-                '#Issues': sleep_wrapper(repo.get_issues, state='all').totalCount,
-                '#Pull Requests': sleep_wrapper(repo.get_pulls, state='all').totalCount,
                 '#Branches': sleep_wrapper(repo.get_branches).totalCount,
                 '#Commits': sleep_wrapper(repo.get_commits).totalCount,
                 '#Comments': sleep_wrapper(repo.get_comments).totalCount,
-                '#Deployments': sleep_wrapper(repo.get_deployments).totalCount
+                '#Deployments': sleep_wrapper(repo.get_deployments).totalCount,
+                '#Pull Requests': sleep_wrapper(repo.get_pulls, state='all').totalCount,
+                '#Pull Requests (Open)': sleep_wrapper(repo.get_pulls, state='open').totalCount,
+                '#Issues': issues.totalCount,
+                '#Issues (Open)': sleep_wrapper(repo.get_issues, state='open').totalCount
             }
+            
+            if issues.totalCount > 0:
+                # An issue link example: https://api.github.com/repos/microsoft/Partner-Center-Java-Samples/issues/30
+                repo_data['#Issues (All)'] = issues[0].number
+            else:
+                repo_data['#Issues (All)'] = 0
 
-            if name is not None:
-                repo_data['Name'] = name
+            if releases.totalCount > 0:
+                repo_data['First Release Date'] = pd.to_datetime(
+                    releases.reversed[0].created_at).to_datetime64()
+
+            return repo_data, None
 
         except Exception as err:
-            error_data = {'Repo': repo_name, 'Error': str(err)}
+            error_data = {'Repo': repo_name, 'Error': err}
             return None, error_data
 
-        return repo_data, None
+    def collect(self, repo_list, tool_release_date=None):
+        errors_data = None
+        repos_data = None
 
-    def collect(self, repo_names, name=None, date=None):
-        repos_data = pd.DataFrame(columns=self.repo_columns)
-        errors_data = pd.DataFrame(columns=self.error_columns)
-
-        for repo_name in repo_names:
+        for repo_name in repo_list:
             repo_data, error_data = self.scrape(
-                repo_name=repo_name, name=name, date=date)
-
+                repo_name=repo_name, tool_release_date=tool_release_date)
             if error_data is None:
                 repo_data = pd.DataFrame([repo_data])
                 repos_data = pd.concat(
@@ -96,5 +80,5 @@ class GitHubMiner():
                 error_data = pd.DataFrame([error_data])
                 errors_data = pd.concat(
                     [errors_data, error_data], ignore_index=True)
-            
+
         return repos_data, errors_data
--- Code/GLMiner.py
+++ Code/GLMiner.py
@@ -1,94 +1,79 @@
-import gitlab
-import operator
+from gitlab import Gitlab
 import pandas as pd
+import operator
+import time
 
 
 class GitLabMiner():
     def __init__(self, private_token):
-        self.gitlab = gitlab.Gitlab(private_token=private_token)
-        self.repo_columns = [
-            'Repo',
-            'Link',
-            'Creation Date',
-            'First Activity Date',
-            'Last Activity Date',
-            'Language',
-            'Topics',
-            'Archived',
-            '#Star',
-            '#Fork',
-            '#Issues',
-            '#Commits',
-            '#Branches',
-            '#Releases',
-            '#Member'
-        ]
-        self.error_columns = [
-            'Repo',
-            'Error'
-        ]
+        self.gitlab = Gitlab(private_token=private_token)
 
-    def scrape(self, repo_name, name=None, date=None):
+    def scrape(self, repo_name, tool_release_date=None):
         def sleep_wrapper(func, **args):
+            time.sleep(0.1)
             return func(**args)
+        
         try:
-            project = self.gitlab.projects.get(repo_name)
+            repo = self.gitlab.projects.get(repo_name)
 
-            commits = sleep_wrapper(project.commits.list, all=True)
+            commits = sleep_wrapper(repo.commits.list, get_all=True)
             last_commit_date = pd.to_datetime(
                 commits[0].created_at).to_datetime64()
-            if date is not None and date > last_commit_date:
+            if tool_release_date is not None and tool_release_date > last_commit_date:
                 error_data = {'Repo': repo_name, 'Error': 'Unrelevant'}
                 return None, error_data
 
+            issues = sleep_wrapper(repo.issues.list, get_all=True, state='all')
+
             repo_data = {
                 'Repo': repo_name,
-                'Link': project.http_url_to_repo,
-                'Creation Date': pd.to_datetime(project.created_at).to_datetime64(),
-                'First Activity Date': pd.to_datetime(commits[-1].created_at).to_datetime64(),
-                'Last Activity Date': last_commit_date,
-                'Language': max(sleep_wrapper(project.languages).items(), key=operator.itemgetter(1))[0],
-                'Topics': project.topics,
-                'Archived': project.archived,
-                '#Star': project.star_count,
-                '#Fork': project.forks_count,
-                '#Issues': len(sleep_wrapper(project.issues.list, all=True)),
-                '#Commits': len(sleep_wrapper(project.commits.list, all=True)),
-                '#Branches': len(sleep_wrapper(project.branches.list, all=True)),
-                '#Releases': len(sleep_wrapper(project.releases.list, all=True)),
-                '#Member': len(sleep_wrapper(project.members.list, all=True))
+                'Link': repo.http_url_to_repo,
+                'Repo Creation Date': pd.to_datetime(repo.created_at).to_datetime64(),
+                'Last Commit Date': last_commit_date,
+                'Topics': repo.topics,
+                'Language': max(sleep_wrapper(repo.languages).items(), key=operator.itemgetter(1))[0],
+                '#Star': repo.star_count,
+                '#Fork': repo.forks_count,
+                '#Commits': len(commits),
+                '#Branches': len(sleep_wrapper(repo.branches.list, get_all=True)),
+                '#Releases': len(sleep_wrapper(repo.releases.list, get_all=True)),
+                '#Member': len(sleep_wrapper(repo.members.list, get_all=True)),
+                '#Issues': len(issues),
+                '#Issues (Open)': len(sleep_wrapper(repo.issues.list, get_all=True, state='opened'))
             }
+            
+            if len(issues) > 0:
+                # An issue link example: https://gitlab.com/librespacefoundation/polaris/polaris/-/issues/30
+                repo_data['#Issues (All)'] = issues[0].iid
+            else:
+                repo_data['#Issues (All)'] = 0
 
             try:
-                repo_data['#Merge Requests'] = len(
-                    sleep_wrapper(project.mergerequests.list, all=True))
-            except Exception as err:
+                repo_data['#Merge Requests'] = len(sleep_wrapper(
+                    repo.mergerequests.list, get_all=True, state='all'))
+                repo_data['#Merge Requests (Open)'] = len(sleep_wrapper(
+                    repo.mergerequests.list, get_all=True, state='opened'))
+            except:
                 repo_data['#Merge Requests'] = 0
-
-            if name is not None:
-                repo_data['Name'] = name
+                repo_data['#Merge Requests (Open)'] = 0
+                
+            return repo_data, None
 
         except Exception as err:
-            error_data = {'Repo': repo_name, 'Error': str(err)}
+            error_data = {'Repo': repo_name, 'Error': err}
             return None, error_data
 
-        return repo_data, None
-
-    def collect(self, repo_names, name=None, date=None):
-        repos_data = pd.DataFrame(columns=self.repo_columns)
-        errors_data = pd.DataFrame(columns=self.error_columns)
-
-        for repo_name in repo_names:
-            repo_data, error_data = self.scrape(
-                repo_name=repo_name, name=name, date=date)
-
+    def collect(self, repo_list, tool_release_date=None):
+        errors_data = None
+        repos_data = None
+        
+        for repo_name in repo_list:
+            repo_data, error_data = self.scrape(repo_name=repo_name, tool_release_date=tool_release_date)
             if error_data is None:
                 repo_data = pd.DataFrame([repo_data])
-                repos_data = pd.concat(
-                    [repos_data, repo_data], ignore_index=True)
+                repos_data = pd.concat([repos_data, repo_data], ignore_index=True)
             else:
                 error_data = pd.DataFrame([error_data])
-                errors_data = pd.concat(
-                    [errors_data, error_data], ignore_index=True)
+                errors_data = pd.concat([errors_data, error_data], ignore_index=True)
 
         return repos_data, errors_data
